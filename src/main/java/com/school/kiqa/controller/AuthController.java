ede0ac37d041a38310bfb47b0aa02a76d7fa2532
Filipa <filipatitan@gmail.com>
2022-06-18 22:56:11 +0100

    added cookies on login

--- src/main/java/com/school/kiqa/controller/AuthController.java
+++ src/main/java/com/school/kiqa/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.school.kiqa.command.dto.auth.LoginDto;
 import com.school.kiqa.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,14 +19,29 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final String COOKIE = "cookie_auth";
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDto> login(@Valid @RequestBody CredentialsDto credentialsDto) {
+    public ResponseEntity<LoginDto> login(@Valid @RequestBody CredentialsDto credentials) {
         log.info("Request received to login");
-        LoginDto loggedInUser = authService.login(credentialsDto);
+        final var loggedInUser = authService.login(credentials);
+
+        ResponseCookie cookie = ResponseCookie
+                .from(COOKIE, loggedInUser.getToken())
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(24 * 60 * 60)
+                .path("/")
+                .build();
+        log.info("Set cookie");
+
+
         log.info("User with id {} and role {} logged in",
                 loggedInUser.getPrincipalDto().getId(),
                 loggedInUser.getPrincipalDto().getRole());
-        return ResponseEntity.ok(loggedInUser);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(loggedInUser);
     }
 }
