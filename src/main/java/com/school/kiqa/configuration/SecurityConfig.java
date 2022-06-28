159c1a4b6e9afcf4c1eab74388806160ef45db64
Filipa <filipatitan@gmail.com>
2022-06-28 16:05:58 +0100

    changed cors allowed origins

--- src/main/java/com/school/kiqa/configuration/SecurityConfig.java
+++ src/main/java/com/school/kiqa/configuration/SecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -78,7 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
-        configuration.addAllowedOrigin("*");
+        configuration.setAllowedOrigins(Collections.singletonList("https://kiqa.vercel.app"));
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
