7a12df47875bc433b551f2ad400a16b1b2550feb
Filipa <filipatitan@gmail.com>
2022-06-29 12:13:18 +0100

    fixing error continuation

--- src/main/java/com/school/kiqa/persistence/repository/SessionRepository.java
+++ src/main/java/com/school/kiqa/persistence/repository/SessionRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface SessionRepository extends JpaRepository<SessionEntity, Long> {
 
-    Optional<SessionEntity> findByUuid(String tokenUuid);
+    Optional<SessionEntity> findByTokenUuid(String tokenUuid);
 }
