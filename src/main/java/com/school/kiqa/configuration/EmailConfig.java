6be6ff1b53173369d9ccd3172b13b820208afd9c
Nuno Neto <nunoneto2015@gmail.com>
2022-07-01 12:27:17 +0100

    fixed some error on EmailConfig

--- src/main/java/com/school/kiqa/configuration/EmailConfig.java
+++ src/main/java/com/school/kiqa/configuration/EmailConfig.java
@@ -33,6 +33,6 @@ public class EmailConfig {
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
-        return mailSender();
+        return mailSender;
     }
 }
