4d6d281e72dedcd9a66b573a4e823665d33bf4f0
Filipa <filipatitan@gmail.com>
2022-07-01 01:05:13 +0100

    added images to frontend

--- src/main/java/com/school/kiqa/controller/WelcomeController.java
+++ src/main/java/com/school/kiqa/controller/WelcomeController.java
@@ -17,17 +17,40 @@ public class WelcomeController {
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Frontend 4 BE</title>\n" +
+                "    <title>FE 4 BE</title>\n" +
                 "    <style>\n" +
                 "        body {\n" +
                 "            display: flex;\n" +
                 "            align-items: center;\n" +
                 "            justify-content: center;\n" +
-                "            font-family: Courier new, monospace\n" +
+                "            font-family: Courier new, monospace;\n" +
+                "            flex-direction: column;\n" +
                 "        }\n" +
                 "\n" +
                 "        h1 {\n" +
                 "            text-align: center\n" +
+                "        }\n" +
+                "\n" +
+                "        .photos {\n" +
+                "            display: flex;\n" +
+                "            align-items: center;\n" +
+                "            justify-content: center;\n" +
+                "        }\n" +
+                "\n" +
+                "        .identify {\n" +
+                "            display: flex;\n" +
+                "            align-items: center;\n" +
+                "            justify-content: center;\n" +
+                "            flex-direction: column;\n" +
+                "            margin: 5px;\n" +
+                "        }\n" +
+                "\n" +
+                "        .names {\n" +
+                "            color: darkblue;\n" +
+                "        }\n" +
+                "\n" +
+                "        img {\n" +
+                "            max-height: 300px;\n" +
                 "        }\n" +
                 "    </style>\n" +
                 "</head>\n" +
@@ -40,6 +63,19 @@ public class WelcomeController {
                 "    <p>Besides that users can also check out our products.</p>\n" +
                 "    <p>You can check the products at <b>GET /products</b></p>\n" +
                 "    <p>Check everything else you can do at <b>GET /swagger-ui/index.html#/</b></p>\n" +
+                "</div>\n" +
+                "<h3>Our beloved backenders</h3>\n" +
+                "<div class=\"photos\">\n" +
+                "    <div class=\"identify\">\n" +
+                "        <img src=\"https://media.discordapp.net/attachments/890702053234589696/992217691521830992/20220607_233603_1.jpg?width=658&height=966\"\n" +
+                "             alt=\"Flepa\">\n" +
+                "        <p class=\"names\">Flepa</p>\n" +
+                "    </div>\n" +
+                "    <div class=\"identify\">\n" +
+                "        <img src=\"https://media.discordapp.net/attachments/890702053234589696/992217730457546862/PHOTO-2022-07-01-00-58-18.jpg\"\n" +
+                "             alt=\"Neto\">\n" +
+                "        <p class=\"names\">Neto</p>\n" +
+                "    </div>\n" +
                 "</div>\n" +
                 "</body>\n" +
                 "</html>");
